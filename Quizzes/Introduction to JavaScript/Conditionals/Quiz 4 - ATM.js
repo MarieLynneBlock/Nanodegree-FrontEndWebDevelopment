17e7576f2897e568dc696dc9ec28a7f6b3db37b2
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-05 21:24:06 +0100

    Update Quiz 4 - ATM.js

--- Quizzes/Introduction to JavaScript/Conditionals/Quiz 4 - ATM.js
+++ Quizzes/Introduction to JavaScript/Conditionals/Quiz 4 - ATM.js
@@ -1,4 +1,4 @@
-//*
+/*
 QUIZ 4:
 
 Using the flowchart below, write the code to represent checking your balance at the ATM. The yellow diamonds represent conditional statements and the blue rectangles with rounded corners represent what should be printed to the console.
@@ -9,7 +9,7 @@ Use the following variables in your solution:
 balance - the account balance
 isActive - if account is active
 checkBalance - if you want to check balance
-*//
+*/
 
 
 // change the values of `balance`, `checkBalance`, and `isActive` to test your code
