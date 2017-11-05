07e9f9405c2b52ac103620ba35cc5c72277baac1
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-05 21:22:53 +0100

    Create ATM.js

---
+++ Quizzes/Introduction to JavaScript/Conditionals/ATM.js
@@ -0,0 +1,35 @@
+//*
+QUIZ 4:
+
+Using the flowchart below, write the code to represent checking your balance at the ATM. The yellow diamonds represent conditional statements and the blue rectangles with rounded corners represent what should be printed to the console.
+
+Flowchart for checking your balance at the ATM (Click the image to enlarge the flowchart).
+Use the following variables in your solution:
+
+balance - the account balance
+isActive - if account is active
+checkBalance - if you want to check balance
+*//
+
+
+// change the values of `balance`, `checkBalance`, and `isActive` to test your code
+var balance = 325.00;
+var checkBalance = true;
+var isActive = true;
+
+  
+if (balance > 0 && isActive === true) {
+    console.log("Your balance is $" + balance.toFixed(2)+".");
+}
+
+else if (isActive === false) {
+    console.log("Your account is no longer active."); 
+}
+    
+else if (balance < 0 && isActive === true) {
+    console.log("Your balance is negative. Please contact bank.");
+}
+
+else {
+    console.log("Thank you. Have a nice day!");
+}
