dd4bbb00ca4ffe46fb714c69a36763eb4163c9c7
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-05 20:32:57 +0100

    Create EvenOdd.js

---
+++ Quizzes/Introduction to JavaScript/Conditionals/EvenOdd.js
@@ -0,0 +1,18 @@
+/*
+QUIZ: 
+
+Write an if..else statement that:
+prints "even" if the number is an even number
+prints "odd" if the number is an odd number
+*/
+
+
+var number = 12;  //change value to test code
+
+if (number % 2 === 0) {
+    console.log("even");
+}
+
+else {
+ console.log("odd");
+}
