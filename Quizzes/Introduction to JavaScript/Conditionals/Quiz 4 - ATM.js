8bd47711b1f6d5492416d0a0fac69247fbe3ce1b
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-05 21:45:28 +0100

    Update Quiz 4 - ATM.js

--- Quizzes/Introduction to JavaScript/Conditionals/Quiz 4 - ATM.js
+++ Quizzes/Introduction to JavaScript/Conditionals/Quiz 4 - ATM.js
@@ -18,18 +18,26 @@ var checkBalance = true;
 var isActive = true;
 
   
-if (balance > 0 && isActive === true) {
-    console.log("Your balance is $" + balance.toFixed(2)+".");
-}
-
-else if (isActive === false) {
+if(checkBalance){
+  
+  if(isActive === true && balance > 0){ 
+    console.log("Your balance is $"+ balance.toFixed(2) +"."); 
+  }
+  
+  else if (isActive === true && balance === 0 ){ 
+    console.log("Your account is empty.");
+  } 
+  
+  else if (isActive === false){ 
     console.log("Your account is no longer active."); 
-}
-    
-else if (balance < 0 && isActive === true) {
-    console.log("Your balance is negative. Please contact bank.");
+  }
+  
+  else { 
+      console.log("Your balance is negative. Please contact bank."); 
+  }
+  
 }
 
-else {
-    console.log("Thank you. Have a nice day!");
+else { 
+  console.log("Thank you. Have a nice day!"); 
 }
