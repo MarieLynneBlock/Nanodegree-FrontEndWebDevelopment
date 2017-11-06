2cd1ed54a0732a46f1590e4f126daeb061eecdd2
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-06 22:02:28 +0100

    Create Quiz 7 - FoodChain.js

---
+++ Quizzes/1. Introduction to JavaScript/1.1 Conditionals/Quiz 7 - FoodChain.js
@@ -0,0 +1,21 @@
+/*
+ * Programming Quiz - Navigating the Food Chain (3-8)
+ *
+ * Use a series of ternary operator to set the category to one of the following:
+ *   - "herbivore" if an animal eats plants
+ *   - "carnivore" if an animal eats animals
+ *   - "omnivore" if an animal eats plants and animals
+ *   - undefined if an animal doesn't eat plants or animals
+ *
+ * Notes
+ *   - use the variables `eatsPlants` and `eatsAnimals` in your ternary expressions
+ *   - `if` statements aren't allowed ;-)
+ */
+
+// change the values of `eatsPlants` and `eatsAnimals` to test your code
+var eatsPlants = false;
+var eatsAnimals = true;
+
+var category = /* your code goes here */
+
+console.log(category);
