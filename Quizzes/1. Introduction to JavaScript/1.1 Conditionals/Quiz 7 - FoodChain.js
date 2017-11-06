f7a42bbee7f46f8c10539c80070bfb85daa1f387
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-06 22:08:54 +0100

    Update Quiz 7 - FoodChain.js

--- Quizzes/1. Introduction to JavaScript/1.1 Conditionals/Quiz 7 - FoodChain.js
+++ Quizzes/1. Introduction to JavaScript/1.1 Conditionals/Quiz 7 - FoodChain.js
@@ -16,6 +16,5 @@
 var eatsPlants = false;
 var eatsAnimals = true;
 
-var category = /* your code goes here */
-
+var category = (eatsPlants && eatsAnimals) ? "omnivore" : (eatsAnimals) ? "carnivore" : (eatsPlants) ? "herbivore" : undefined ;
 console.log(category);
