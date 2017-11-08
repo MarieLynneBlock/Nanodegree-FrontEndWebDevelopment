8df18ca43ea0abbf87f85f4d4ca6110cb0fba502
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-08 20:11:11 +0100

    Update Quiz 10 - BottlesOfJuice.js

--- Quizzes/1. Introduction to JavaScript/1.2 Loops/Quiz 10 - BottlesOfJuice.js
+++ Quizzes/1. Introduction to JavaScript/1.2 Loops/Quiz 10 - BottlesOfJuice.js
@@ -17,9 +17,19 @@ Some Notes:
 
 var num = 99;
 
-while (/* your stop condition goes here */) {
-    // check value of num
-    // print lyrics using num
-    // don't forget to check pluralization on the last line!
-    // decrement num
+while ( num >= 1) {
+    
+    if (num > 2) {
+    console.log(num +" bottles of juice on the wall! " + num +" bottles of juice! Take one down, pass it around... "+ (num -1) + " bottles of juice on the wall!");
+    }
+    
+    else if(num === 2) {
+        console.log(num +" bottles of juice on the wall! "+ num +" bottles of juice! Take one down, pass it around... "+ (num -1) + " bottle of juice on the wall!");
+    }
+    
+    else {
+        console.log(num +" bottle of juice on the wall! "+ num +" bottle of juice! Take one down, pass it around... "+ (num -1) + " bottles of juice on the wall!");
+    }
+    
+    num --;
 }
