9e77013318da372a4b5fad64e17719bd97873cbf
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-08 20:11:59 +0100

    Update Quiz 10 - BottlesOfJuice.js

--- Quizzes/1. Introduction to JavaScript/1.2 Loops/Quiz 10 - BottlesOfJuice.js
+++ Quizzes/1. Introduction to JavaScript/1.2 Loops/Quiz 10 - BottlesOfJuice.js
@@ -20,15 +20,15 @@ var num = 99;
 while ( num >= 1) {
     
     if (num > 2) {
-    console.log(num +" bottles of juice on the wall! " + num +" bottles of juice! Take one down, pass it around... "+ (num -1) + " bottles of juice on the wall!");
+    console.log(num + " bottles of juice on the wall! " + num + " bottles of juice! Take one down, pass it around... " + (num -1) + " bottles of juice on the wall!");
     }
     
     else if(num === 2) {
-        console.log(num +" bottles of juice on the wall! "+ num +" bottles of juice! Take one down, pass it around... "+ (num -1) + " bottle of juice on the wall!");
+        console.log(num + " bottles of juice on the wall! "+ num + " bottles of juice! Take one down, pass it around... " + (num -1) + " bottle of juice on the wall!");
     }
     
     else {
-        console.log(num +" bottle of juice on the wall! "+ num +" bottle of juice! Take one down, pass it around... "+ (num -1) + " bottles of juice on the wall!");
+        console.log(num + " bottle of juice on the wall! "+ num + " bottle of juice! Take one down, pass it around... " + (num -1) + " bottles of juice on the wall!");
     }
     
     num --;
