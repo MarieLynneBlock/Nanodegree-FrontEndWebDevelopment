95d685ae5b0101f1eadb634cf3cc0a26a169703c
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-08 19:50:35 +0100

    Create Quiz 10 - BottlesOfJuice.js

---
+++ Quizzes/1. Introduction to JavaScript/1.2 Loops/Quiz 10 - BottlesOfJuice.js
@@ -0,0 +1,25 @@
+/*
+QUIZ 10:
+
+Write a loop that prints out the following song. Starting at 99, and ending at 1 bottle.
+
+99 bottles of juice on the wall! 99 bottles of juice! Take one down, pass it around... 98 bottles of juice on the wall!
+98 bottles of juice on the wall! 98 bottles of juice! Take one down, pass it around... 97 bottles of juice on the wall!
+...
+2 bottles of juice on the wall! 2 bottles of juice! Take one down, pass it around... 1 bottle of juice on the wall!
+1 bottle of juice on the wall! 1 bottle of juice! Take one down, pass it around... 0 bottles of juice on the wall!
+Some Notes:
+
+* Note the pluralization of the word "bottle" when you go from 2 bottles to 1 bottle.
+* Your text editor may try to autocorrect your ellipses ... to the ellipses character … Do not use the ellipses character for this quiz.
+*/
+
+
+var num = 99;
+
+while (/* your stop condition goes here */) {
+    // check value of num
+    // print lyrics using num
+    // don't forget to check pluralization on the last line!
+    // decrement num
+}
