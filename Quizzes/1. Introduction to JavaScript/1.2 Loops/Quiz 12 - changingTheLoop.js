3e3fbd2302292e8e1698afcf3a7d010b300ca93d
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-11 22:38:46 +0100

    Create Quiz 12 - changingTheLoop.js

---
+++ Quizzes/1. Introduction to JavaScript/1.2 Loops/Quiz 12 - changingTheLoop.js
@@ -0,0 +1,17 @@
+/*
+QUIZ 12:
+
+rewrite the while loop as a for loop
+
+
+var x = 9;
+while (x >= 1) {
+    console.log("hello " + x);
+    x = x - 1;
+}
+*/
+
+
+for (var x = 9; x >= 1; x--) {
+    console.log("hello " + x);
+}
