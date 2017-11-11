ad0e8fd09e000c178d2a8a4c842808af999cc61a
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-11 22:41:09 +0100

    Create Quiz 13 - Debug.js

---
+++ Quizzes/1. Introduction to JavaScript/1.2 Loops/Quiz 13 - Debug.js
@@ -0,0 +1,16 @@
+/*
+QUIZ 13:
+
+Here is a for loop that's supposed to print the numbers 5 through 9. Fix the error!
+
+
+for (x < 10; x++) {
+  console.log(x);
+}
+*/
+
+
+for (var x = 5; x < 10; x++) {
+    console.log(x);
+}
+
