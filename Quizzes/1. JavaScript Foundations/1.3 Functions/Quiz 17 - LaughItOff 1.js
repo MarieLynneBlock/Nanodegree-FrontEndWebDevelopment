f59823a95377292a9f2f1f502187325bbd0e1f24
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-12 21:58:43 +0100

    Create Quiz 17 - LaughItOff 1.js

---
+++ Quizzes/1. JavaScript Foundations/1.3 Functions/Quiz 17 - LaughItOff 1.js
@@ -0,0 +1,12 @@
+/*
+QUIZ 17:
+
+Declare a function called laugh() that returns "hahahahahahahahahaha!".
+Print the value returned from the laugh() function to the console.
+*/
+
+function laugh() {
+    return "hahahahahahahahahaha!";
+}
+
+console.log(laugh());
