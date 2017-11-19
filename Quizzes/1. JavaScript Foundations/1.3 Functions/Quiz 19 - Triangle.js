387bc90c1267886a8f3cc537b343acf27557572c
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-19 14:27:18 +0100

    Create Quiz 19 - Triangle.js

---
+++ Quizzes/1. JavaScript Foundations/1.3 Functions/Quiz 19 - Triangle.js
@@ -0,0 +1,54 @@
+/*
+QUIZ 19:
+
+For this quiz, you're going to create a function called buildTriangle() that will accept an input (the triangle at its widest width) and will return the string representation of a triangle. See the example output below.
+
+buildTriangle(10);
+Returns:
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+* * * * * * 
+* * * * * * * 
+* * * * * * * * 
+* * * * * * * * * 
+* * * * * * * * * *
+We've given you one function makeLine() to start with. The function takes in a line length, and builds a line of asterisks and returns the line with a newline character.
+
+  function makeLine(length) {
+    var line = "";
+    for (var j = 1; j <= length; j++) {
+      line += "* "
+    }
+    return line + "\n";
+  }
+  
+You will need to call this makeLine() function in buildTriangle().
+
+This will be the most complicated program you've written yet, so take some time thinking through the problem before diving into the code. What tools will you need from your JavaScript tool belt? Professionals plan out their code before writing anything. Think through the steps your code will need to take and write them down in order. Then go through your list and convert each step into actual code. Good luck!
+*/
+
+
+// creates a line of * for a given length
+function makeLine(length) {
+    var line = "";
+    for (var j = 1; j <= length; j++) {
+        line += "* ";
+    }
+    return line + "\n";
+}
+
+function buildTriangle(width) {
+  var triangle = "";
+  
+  for (i = 0; i <= width; i++) {
+    triangle += makeLine(i);
+    console.log(makeLine(i));
+  }
+}
+
+// test your code by uncommenting the following line
+//console.log(buildTriangle(10));
