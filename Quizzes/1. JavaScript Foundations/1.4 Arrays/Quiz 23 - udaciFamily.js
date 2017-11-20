56fe8f8804d790fd0f8633b51cb7646613086007
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-20 18:28:02 +0100

    Create Quiz 23 - udaciFamily.js

---
+++ Quizzes/1. JavaScript Foundations/1.4 Arrays/Quiz 23 - udaciFamily.js
@@ -0,0 +1,10 @@
+/*
+QUIZ 23:
+
+Directions:
+Create an array called udaciFamily and add "Julia", "James", and your name to the array.
+Then, print the udaciFamily to the console using console.log.
+*/
+
+var udaciFamily = ["Julia", "James", "Lien"];
+  console.log(udaciFamily);
