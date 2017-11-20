d45c5e7dfd8581cdc24188fe75bc28753835c449
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-20 18:29:36 +0100

    Create Quiz 24 - price.js

---
+++ Quizzes/1. JavaScript Foundations/1.4 Arrays/Quiz 24 - price.js
@@ -0,0 +1,23 @@
+/*
+QUIZ 24:
+
+Directions:
+Starting with this array of prices, change the prices of the 1st, 3rd, and 7th elements in the array.
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+
+Afterwards, print out the prices array to the console.
+*/
+
+
+var prices = [1.23, 48.11, 90.11, 8.50, 9.99, 1.00, 1.10, 67.00];
+
+
+prices[0] = 1;
+prices[2] = 3;
+prices[6] = 7;
+
+console.log(prices);
+
+
