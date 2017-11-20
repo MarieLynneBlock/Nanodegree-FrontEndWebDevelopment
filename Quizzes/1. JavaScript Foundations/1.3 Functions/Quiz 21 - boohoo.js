9e85df031441d85307e274140a82868e6c1944e7
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-20 01:31:42 +0100

    Create Quiz 21 - boohoo.js

---
+++ Quizzes/1. JavaScript Foundations/1.3 Functions/Quiz 21 - boohoo.js
@@ -0,0 +1,16 @@
+/*
+QUIZ 21:
+
+Directions:
+Write a named function expression that stores the function in a variable called cry and returns "boohoo!".
+Don't forget to call the function using the variable name, not the function name:
+
+cry();
+Returns: boohoo!
+*/
+
+var cry = function sad(){
+    return "boohoo!";
+}; 
+
+console.log (cry());
