5103a16e971a530c5fad82198b6f69fe73f473a3
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-26 14:01:30 +0100

    Create Quiz 29 - forEachLoop.js

---
+++ Quizzes/1. JavaScript Foundations/Quiz 29 - forEachLoop.js
@@ -0,0 +1,26 @@
+/*
+QUIZ 29:
+
+Directions:
+Use the array's forEach() method to loop over the following array and add 100 to each of the values if the value is divisible by 3.
+
+var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4, 19, 300, 3775, 299, 36, 209, 148, 169, 299, 6, 109, 20, 58, 139, 59, 3, 1, 139];
+Remember that the "Test Run" button will display any logged content, so feel free to use console.log() to test your code.
+
+
+Use the existing `test` variable and write a `forEach` loop
+that adds 100 to each number that is divisible by 3.
+
+
+Things to note:
+- you must use an `if` statement to verify code is divisible by 3
+- you can use `console.log` to verify the `test` variable when you're finished looping
+*/
+
+
+var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4, 19, 300, 3775, 299, 36, 209, 148, 169, 299, 6, 109, 20, 58, 139, 59, 3, 1, 139];
+
+
+
+
+
