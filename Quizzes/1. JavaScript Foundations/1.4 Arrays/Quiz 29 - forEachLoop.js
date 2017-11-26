1ce56e420ed487c35a5075fde970df8cbca68a34
Marie-Lynne Block <marielynne.block@gmail.com>
2017-11-26 14:03:54 +0100

    Update Quiz 29 - forEachLoop.js

--- Quizzes/1. JavaScript Foundations/1.4 Arrays/Quiz 29 - forEachLoop.js
+++ Quizzes/1. JavaScript Foundations/1.4 Arrays/Quiz 29 - forEachLoop.js
@@ -21,6 +21,13 @@ Things to note:
 var test = [12, 929, 11, 3, 199, 1000, 7, 1, 24, 37, 4, 19, 300, 3775, 299, 36, 209, 148, 169, 299, 6, 109, 20, 58, 139, 59, 3, 1, 139];
 
 
+test.forEach(function(num, index, array) {
+    if(num % 3 === 0) {
+    array[index] = num += 100; 
+  }
+});
+
+console.log(test);
 
 
 
