2b96b32a3ed44e8b8d1585cdddc32d7fc31b526e
Marie-Lynne Block <marielynne.block@gmail.com>
2017-12-31 02:05:23 +0100

    Create Quiz 31 - Nested.js

---
+++ Quizzes/1. JavaScript Foundations/1.4 Arrays/Quiz 31 - Nested.js
@@ -0,0 +1,28 @@
+/*
+ * Programming Quiz: Nested Numbers (6-10)
+ *
+ *   - The `numbers` variable is an array of arrays.
+ *   - Use a nested `for` loop to cycle through `numbers`.
+ *   - Convert each even number to the string "even"
+ *   - Convert each odd number to the string "odd"
+ */
+
+var numbers = [
+    [243, 12, 23, 12, 45, 45, 78, 66, 223, 3],
+    [34, 2, 1, 553, 23, 4, 66, 23, 4, 55],
+    [67, 56, 45, 553, 44, 55, 5, 428, 452, 3],
+    [12, 31, 55, 445, 79, 44, 674, 224, 4, 21],
+    [4, 2, 3, 52, 13, 51, 44, 1, 67, 5],
+    [5, 65, 4, 5, 5, 6, 5, 43, 23, 4424],
+    [74, 532, 6, 7, 35, 17, 89, 43, 43, 66],
+    [53, 6, 89, 10, 23, 52, 111, 44, 109, 80],
+    [67, 6, 53, 537, 2, 168, 16, 2, 1, 8],
+    [76, 7, 9, 6, 3, 73, 77, 100, 56, 100]
+];
+
+// your code goes here
+for (var row = 0; row < numbers.length; row++) {
+    for (var column = 0; column < numbers[row].length; column++) {
+        
+    }
+}
