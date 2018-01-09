07b3674de5ee43bc993dc01a24195f8c1ef46c87
Marie-Lynne Block <marielynne.block@gmail.com>
2018-01-09 23:28:12 +0100

    Create Quiz 33 - Breakfast.js

---
+++ Quizzes/1. JavaScript/1.5 Objects/Quiz 33 - Breakfast.js
@@ -0,0 +1,14 @@
+/*
+Create a breakfast object to represent the following menu item:
+
+The Lumberjack - $9.95
+eggs, sausage, toast, hashbrowns, pancakes
+The object should contain properties for the name, price, and ingredients.
+*/
+
+
+breakfast = {
+    name: "The Lumberjack",
+    price: "$9.95",
+    ingredients: ["eggs", "sausage", "toast", "hashbrowns", "pancakes"]
+};
