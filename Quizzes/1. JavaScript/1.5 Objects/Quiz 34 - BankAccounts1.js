5c444dc554700fec6ebcf4c584f9a48062919975
Marie-Lynne Block <marielynne.block@gmail.com>
2018-01-09 23:56:31 +0100

    Create Quiz 34 - BankAccounts1.js

---
+++ Quizzes/1. JavaScript/1.5 Objects/Quiz 34 - BankAccounts1.js
@@ -0,0 +1,48 @@
+/*
+ Using the given object:
+
+var savingsAccount = {
+  balance: 1000,
+  interestRatePercent: 1,
+  deposit: function addMoney(amount) {
+    if (amount > 0) {
+      savingsAccount.balance += amount;
+    }
+  },
+  withdraw: function removeMoney(amount) {
+    var verifyBalance = savingsAccount.balance - amount;
+    if (amount > 0 && verifyBalance >= 0) {
+      savingsAccount.balance -= amount;
+    }
+  }
+};
+add a printAccountSummary() method that returns the following account message:
+
+  Welcome!
+  Your balance is currently $1000 and your interest rate is 1%.
+ */
+ 
+
+var savingsAccount = {
+    balance: 1000,
+    interestRatePercent: 1,
+    deposit: function addMoney(amount) {
+        if (amount > 0) {
+            savingsAccount.balance += amount;
+        }
+    },
+    
+    withdraw: function removeMoney(amount) {
+        var verifyBalance = savingsAccount.balance - amount;
+        if (amount > 0 && verifyBalance >= 0) {
+            savingsAccount.balance -= amount;
+        }
+    },
+    
+    printAccountSummary() {
+        return("Welcome!\nYour balance is currently $" + savingsAccount.balance +
+            " and your interest rate is "+ savingsAccount.interestRatePercent + "%."); 
+    },
+};
+
+console.log(savingsAccount.printAccountSummary());
