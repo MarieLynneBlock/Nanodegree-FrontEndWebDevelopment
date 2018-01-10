b8bda7792ad88b4fda423990a4f61983fa1c607e
Marie-Lynne Block <marielynne.block@gmail.com>
2018-01-10 14:39:30 +0100

    Update and rename Quizzes/1. JavaScript Foundations/1.2 Loops/Quiz 15 - Factorials.js to Quizzes/1. JavaScript/1.2 Loops/Quiz 15 - Factorials.js

--- Quizzes/1. JavaScript/1.2 Loops/Quiz 15 - Factorials.js
+++ Quizzes/1. JavaScript/1.2 Loops/Quiz 15 - Factorials.js
@@ -3,7 +3,8 @@ QUIZ 15:
 
 Write a for loop that prints out the factorial of the number 12:
 
-A factorial is calculated by multiplying a number by all the numbers below it. For instance, 3! or "3 factorial" is 3 * 2 * 1 = 6
+A factorial is calculated by multiplying a number by all the numbers below it.
+For instance, 3! or "3 factorial" is 3 * 2 * 1 = 6
 
 3! = 3 * 2 * 1 = 6
 4! = 4 * 3 * 2 * 1 = 24
