dae61882e1a28d9d31d55934c6c4ec2218f485ab
Marie-Lynne Block <marielynne.block@gmail.com>
2018-01-10 01:05:15 +0100

    Create Quiz 36 - DonutsRevisited.js

---
+++ Quizzes/1. JavaScript/1.5 Objects/Quiz 36 - DonutsRevisited.js
@@ -0,0 +1,30 @@
+/*
+Here is an array of donut objects.
+
+var donuts = [
+  { type: "Jelly", cost: 1.22 },
+  { type: "Chocolate", cost: 2.45 },
+  { type: "Cider", cost: 1.59 },
+  { type: "Boston Cream", cost: 5.99 }
+];
+
+
+Use the forEach() method to loop over the array and print out the following donut summaries using console.log.
+
+Jelly donuts cost $1.22 each
+Chocolate donuts cost $2.45 each
+Cider donuts cost $1.59 each
+Boston Cream donuts cost $5.99 each
+*/
+
+
+var donuts = [
+    { type: "Jelly", cost: 1.22 },
+    { type: "Chocolate", cost: 2.45 },
+    { type: "Cider", cost: 1.59 },
+    { type: "Boston Cream", cost: 5.99 }
+];
+
+donuts.forEach(function(donut) {
+  console.log(donut.type + " donuts cost $" + donut.cost + " each"); 
+});
