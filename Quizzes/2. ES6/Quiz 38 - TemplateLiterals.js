65360ea417c117c2f54fb0360fb370cdbdacbe2e
Marie-Lynne Block <marielynne.block@gmail.com>
2018-01-11 23:24:23 +0100

    Create Quiz 38 - TemplateLiterals.js

---
+++ Quizzes/2. ES6/Quiz 38 - TemplateLiterals.js
@@ -0,0 +1,15 @@
+/*
+QUIZ 38: Template Literals
+
+Change the greeting string below to use a template literal. Also, feel free to swap in your name for the placeholder.
+
+
+const myName = '[NAME]';
+const greeting = 'Hello, my name is ' + myName;
+console.log(greeting);
+*/
+
+
+const myName = 'Lien';
+const greeting = `Hello, my name is ${myName}`;
+console.log(greeting);
