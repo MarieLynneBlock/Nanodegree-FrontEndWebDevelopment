f99c1c618ae8984e038471b73b87ff984dd56096
Marie-Lynne Block <marielynne.block@gmail.com>
2018-01-12 03:46:58 +0100

    Create Quiz 43 - RestParameter.js

---
+++ Quizzes/2. ES6/Quiz 43 - RestParameter.js
@@ -0,0 +1,27 @@
+/*
+QUIZ 43: Using the Rest Parameter (1-5)
+
+Use the rest parameter to create an average() function that calculates the average of an unlimited amount of numbers.
+
+TIP: Make sure to test your code with different values. For example,
+
+average(2, 6) should return 4
+average(2, 3, 3, 5, 7, 10) should return 5
+average(7, 1432, 12, 13, 100) should return 312.8
+average() should return 0
+*/
+
+
+function average(...nums) {
+    let x = 0;
+    for (const num of nums) {
+        x += num;
+        y = nums.length;
+    }
+    return x/y;
+}
+
+console.log(average(2, 6));
+console.log(average(2, 3, 3, 5, 7, 10));
+console.log(average(7, 1432, 12, 13, 100));
+console.log(average());
